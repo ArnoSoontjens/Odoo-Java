1935ac94261139cc4817cb7a6222895bff1b367e
ArnoSoontjens <arnosoontjens_1@hotmail.com>
2017-03-14 16:20:57 +0100

    Added some (still empty tests)

---
+++ Odoo-Java/src/main/java/com/arnowouter/javaodoo/exceptions/OdooExceptionMessages.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.arnowouter.javaodoo.exceptions;
+
+/**
+ *
+ * @author Arno Soontjens
+ */
+public class OdooExceptionMessages {
+    public static final String EX_MESSAGE_NOT_AUTHENTENTICATED = "Not authenticated. Please call the 'authenticate' function first.";
+}
